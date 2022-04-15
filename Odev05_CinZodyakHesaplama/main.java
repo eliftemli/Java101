195bc53bcd1d3faf9756b18144a1f4dba2b73c05
eliftemli <74073992+eliftemli@users.noreply.github.com>
2022-04-15 16:45:43 +0300

    CinZodyakBulma

---
+++ Odev05_CinZodyakHesaplama/main.java
@@ -0,0 +1,85 @@
+package Odev05_CinZodyakHesaplama;
+
+import java.util.Scanner;
+
+public class main {
+	public static void main(String[] args) {
+		
+		int yil;
+		int kalan;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Doðum Yýlýnýzý Giriniz:");
+		yil = scanner.nextInt();
+		
+		kalan = yil % 12;
+		System.out.println(kalan);
+		
+		switch (kalan) {
+		
+        case 1: {
+			
+			System.out.println("Çin Zodyaðý Burcunuz: Horoz");
+			break;
+		}
+        case 2: {
+			
+			System.out.println("Çin Zodyaðý Burcunuz: Köpek");
+			break;
+		}
+        case 3: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Domuz");
+        	break;
+        }
+        case 4: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Fare");
+        	break;
+        }
+        case 5: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Öküz");
+        	break;
+        }
+        case 6: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Kaplan");
+        	break;
+        }
+        case 7: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Tavþan");
+        	break;
+        }
+        case 8: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Ejderha");
+        	break;
+        }
+        case 9: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Yýlan");
+        	break;
+        }
+        case 10: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: At");
+        	break;
+        }
+        case 11: {
+	
+        	System.out.println("Çin Zodyaðý Burcunuz: Koyun");
+        	break;
+        }
+		default:
+			System.out.println("Çin Zodyaðý Burcunuz: Maymun");
+        	break;
+		}
+		
+		
+		
+	}
+
+}
